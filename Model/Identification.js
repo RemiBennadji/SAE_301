c7304d95fb3c4fe475324acf453452d996a8f3e7
Bastien2004 <bastienhecquet2004@gmail.com>
2024-09-10 19:09:23 +0200

    eye

--- Model/Identification.js
+++ Model/Identification.js
@@ -1,8 +1,14 @@
-function showPassword() {
+document.getElementById('togglePassword').addEventListener('click', function () {
     var passwordField = document.getElementById("password");
+
+    // Bascule entre les types "password" et "text"
     if (passwordField.type === "password") {
         passwordField.type = "text";
+        // Change l'icône en œil barré
+        this.innerHTML = '<i class="fas fa-eye-slash"></i>';
     } else {
         passwordField.type = "password";
+        // Remet l'icône en œil ouvert
+        this.innerHTML = '<i class="fas fa-eye"></i>';
     }
-}
+});
