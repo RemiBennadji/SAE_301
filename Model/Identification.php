e1bc71d4c2fc61efed7a244e623548af9a0a7df3
Bastien2004 <bastienhecquet2004@gmail.com>
2024-09-16 10:40:10 +0200

    Debut identifications

--- Model/Identification.php
+++ Model/Identification.php
@@ -1,5 +1,9 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+    if(isset($_POST["id"]) && isset($_POST["password"])){
+        $id = $_POST["id"];
+        $password = $_POST["password"];
+        echo $id."|".$password;
+    }
 }
 ?>
