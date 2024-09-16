6187ae525ee168208e8b8d05d9e0b86f90b9e616
Noah <syllektis59@gmail.com>
2024-09-16 11:49:37 +0200

    vérif et setter de mot de passe terminé

--- Model/Compte.php
+++ Model/Compte.php
@@ -15,33 +15,39 @@ public function getId(){
 public function getMdp(){
     return $this->mdp;
 }
-public function setMdp($mdp){
+public function verifMdp($mdp)
+{
     $caraSpec = array('!', '.', '€', '@');
     $chiffre = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
     $sec = false;
     $nbrCara = false;
     $nbrChiffre = 0;
-    if(count($mdp) >= 8){
+    if (count($mdp) >= 8) {
         $nbrCara = true;
     }
     foreach ($caraSpec as $cara) {
-        if(in_array($cara, $chiffre)){
+        if (in_array($cara, $chiffre)) {
             $nbrChiffre++;
         }
         if (str_contains($mdp, $cara) == false) {
             continue;
-        }
-        else{
+        } else {
             $sec = true;
         }
     }
-    if($nbrCara == true && $nbrChiffre >= 4 && $sec == true){
+    if ($nbrCara == true && $nbrChiffre >= 1 && $sec == true) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+public function setMdp($mdp){
+    if($this->verifMdp($mdp)){
         $this->mdp = $mdp;
     }
     else{
         echo "Il y a une condition qui n'est pas rempli, veuillez revérifier votre mot de passe.";
     }
-
 }
-
 }
