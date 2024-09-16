afcb1002c2deac603342c7980dfd8097cb9dcf2e
Bastien2004 <bastienhecquet2004@gmail.com>
2024-09-16 09:19:09 +0200

    debut menu

--- Model/Identification.js
+++ Model/Identification.js
@@ -1,13 +1,13 @@
 document.getElementById('togglePassword').addEventListener('click', function () {
-    var passwordField = document.getElementById("password");
+    var password = document.getElementById("password");
 
     // Bascule entre les types "password" et "text"
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
+    if (password.type === "password") {
+        password.type = "text";
         // Change l'icône en œil barré
         this.innerHTML = '<i class="fas fa-eye-slash"></i>';
     } else {
-        passwordField.type = "password";
+        password.type = "password";
         // Remet l'icône en œil ouvert
         this.innerHTML = '<i class="fas fa-eye"></i>';
     }
