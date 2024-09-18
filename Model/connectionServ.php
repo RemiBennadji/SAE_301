2f2af751c2cf222dded1cb14caaf1cdb95389ec9
Dorian Colas <dorian.colas@uphf.fr>
2024-09-18 11:38:40 +0200

    Ajout des requêtes pour avoir les info essetiels

--- Model/connectionServ.php
+++ Model/connectionServ.php
@@ -2,18 +2,32 @@
 //requête SQL
 $sql1 = "SELECT * FROM seance";//idseance typeseance salle code join collège(nom complet prof, matière)
 
+/*
+SELECT idseance, typeseance, salle, collegue.prenom, collegue.nom
+FROM seance
+JOIN collegue ON seance.collegue = collegue.id
+*/
+
+/*
+SELECT idseance, typeseance, salle, collegue.prenom, collegue.nom, enseignement.long
+FROM seance
+JOIN collegue ON seance.collegue = collegue.id
+JOIN enseignement ON enseignement.code = matiere
+*/
+
 // Arguments de connetion
 try {
     $connection = new PDO ("pgsql:host=iutinfo-sgbd.uphf.fr; dbname=edt user=iutinfo340 password=jWBfxD1E");
     $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "connecté.<br>";
+    //echo "connecté.<br>";
 
     $test = $connection -> prepare($sql1);
     $test -> execute();
     $test1= $test -> fetchAll(PDO::FETCH_ASSOC);
     foreach($test1 as $row){
         foreach($row as $key => $value){
+            echo $key.": ".$value."<br>";
             echo $key.": ".$value."<br>";
         }
         echo "<br>";
