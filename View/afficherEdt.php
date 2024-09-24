522993186f2568b5989dc2131af0e401b2d77071
Dorian Colas <dorian.colas@uphf.fr>
2024-09-24 11:14:06 +0200

    - Ajout de la fonction "AfficherCoursClasseHeure" permettant de récupérer et afficher tous les cours d'un groupe selon le groupe et l'horraire.
    - Mise au propre
    
    - Ajout de commentaires
    
    - Test d'une utilisation

--- View/afficherEdt.php
+++ View/afficherEdt.php
@@ -1,106 +1,34 @@
 <?php
 
-global $tabEntier;
-include ('../Model/connectionServ.php');
+function AfficherCoursClasseHeure($heure, $classe){
+    $sql = "
+    SELECT DISTINCT seance.idseance, seance.typeseance, duree, schedule.salle, collegue.prenom, collegue.nom, enseignement.court as matiere, horaire as date, schedule.nomgroupe
+    FROM seance
+    JOIN collegue ON seance.collegue = collegue.id
+    JOIN enseignement ON seance.code = enseignement.code
+    JOIN schedule ON seance.nomgroupe = schedule.nomgroupe
+    WHERE horaire = ? AND schedule.nomgroupe=?";
 
-// Afficher les valeurs pour chaque ligne
+    // Connexion
+    $connexion = new PDO ("pgsql:host=iutinfo-sgbd.uphf.fr; dbname=edt user=iutinfo340 password=jWBfxD1E");
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-/*foreach($tabEntier as $row){
-    foreach($row as $key => $value){
-        echo $key.": ".$value."<br>";
-    }
-    echo "<br>";
-}*/
+    // Préparation de la requête avec les arguments
+    $req = $connexion->prepare($sql);
+    $req -> execute(array($heure,$classe));
+    $req->execute();
 
+    // Récupération des informations
+    $tab = $req->fetchAll(PDO::FETCH_ASSOC);
 
-$minut = 0;//un cours de 1h30 est égal à 90min
-$edt = array();//liste de l'emploi du temps complet
-foreach ($tabEntier as $i){
-    $day = array();//fait la liste de cours pour une journée
-    $lesson = array();//enregistre les id pour verifier si les cours sont deja placé
-    while($minut<=450) {
-        if ((!in_array($i['idseance'], $lesson))) {//450 minutes = 7h30 de cours et verifie si l'id de la seance est deja placé
-            array_push($day, $i);
-            array_push($lesson, $i['idseance']);
-            $day += $i['duree'];
+    // Affichage des informations
+    foreach ($tab as $row) {
+        foreach ($row as $key => $value) {
+            echo $key . " : " . $value . "<br>";
         }
-    }
-    $minut = 0;
-    array_push($edt, $day);
-    array_splice($day);//clear la liste
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Fonction pour afficher l'EDT
-function afficherEDT ($week) {
-    // Dans la semaine, pour chaque jour et heures
-    foreach($week as $day){
-        foreach($day as $lesson){
-            // On affiche toutes les infos du cours (matière, prof, type de cours)
-            afficherInfoHeure($lesson);
-        }
-
+        echo "<br>";
     }
 }
 
-function afficherInfoHeure($lesson){
-    // Il faut retourner ici une case (d'un tableau) qui contient dans l'ordre, le type de séance, la salle, le prof, et la matière
-    // echo ("<td type de séance, la salle, le prof, et la matière>"); // Remplacer par les élement du tableau de cours
-    echo $lesson."<br>";
-}
-
-
-$edtSemaineTest = [[1,2,3]];
-afficherEDT($edtSemaineTest);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+// Test
+AfficherCoursClasseHeure('2025-01-09 08:00:00', 'TDA');
