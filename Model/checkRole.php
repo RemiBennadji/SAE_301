fc9afcff357ad1879814d943ea9e231547440d12
mattheo <etu@>
2024-09-24 10:23:58 +0200

    connexion bdd pour check le role de connexion a l'edt

--- Model/checkRole.php
+++ Model/checkRole.php
@@ -1,1 +1,13 @@
 <?php
+
+
+$sql1 ="select role from infoutilisateur where";
+
+try {
+    $connection = new PDO ("pgsql:host=iutinfo-sgbd.uphf.fr; dbname=edt user=iutinfo340 password=jWBfxD1E");
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+
