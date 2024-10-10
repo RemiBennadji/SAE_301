f4a941a507374ef335838145bb39ce6142cb5098
Noah <syllektis59@gmail.com>
2024-10-10 10:49:08 +0200

    salles vides +vérif et commencement vérif bloc de 3h

--- View/affichageSalle.php
+++ View/affichageSalle.php
@@ -2,22 +2,47 @@
 $jour = $_POST["idJour"];
 $heure = $_POST["idHeure"];
 $timestamp = strtotime($jour);
-
+$horaire = array('8:00', '9:30', '11:00', '12:30', '14:00', '15:30', '17:00');
 // On s'assure que la date est bien au format YYYY-MM-DD (ex : lundi)
 $date = date("Y-m-d", $timestamp).' '.$heure.':00';
 
-//$sql11 ="select role from infoutilisateur where identifiant=:ID and mdp=:PWD";
+for($i=0; $i < count($horaire); $i++) {
+    if($horaire[$i]!=0){
+        if($horaire[$i]==$heure){
+            $heureInf = $horaire[$i-1];
+            $dateInf = date("Y-m-d", $timestamp).' '.$heureInf.':00';
+        }
+    }
+}
+
+
+
 $sql1 ="select distinct salle from schedule where horaire = '$date'";
+$sql2 ="select distinct nosalle from listesalles";
+$sql3 ="select distinct duration from schedule where horaire = '$dateInf'";
+
+$sallesVides = array();
 
 try {
     $connection = new PDO ("pgsql:host=iutinfo-sgbd.uphf.fr; dbname=edt user=iutinfo301 password=YAH+rfI3");
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    $salles = $connection->prepare($sql2);
+    $salles->execute();
+    $sallesDispo = $salles->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($sallesDispo as $nosalle) {
+        $sallesVides[] = $nosalle["nosalle"];
+    }
+
+    echo 'liste salles vides'.'<br>';
+
     $resultSalles = $connection->prepare($sql1);
     $resultSalles->execute();
     $listeSalles =$resultSalles->fetchAll(PDO::FETCH_ASSOC);
     foreach ($listeSalles as $salle) {
-        echo $salle['salle'].'<br>';
+        if(in_array($salle['salle'], $sallesVides)) {
+            echo $salle['salle'].'<br>';
+        }
     }
 
 } catch (PDOException $e) {
