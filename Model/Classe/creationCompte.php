64c92a5f404636f51befa5a7676360de390f71f2
Bastien2004 <bastienhecquet2004@gmail.com>
2024-10-23 10:52:40 +0200

    Debut Import

--- Model/Classe/creationCompte.php
+++ Model/Classe/creationCompte.php
@@ -1,46 +1,11 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fichier = $_FILES["fichier"]["name"];
+    if (pathinfo($_FILES['fichier']['type'], PATHINFO_EXTENSION) === 'csv') {
+        if ((fopen($_FILES['fichier']['type'], "r")) !== FALSE){//Ouverture du fichier
 
-require_once "Compte.php";
-require_once "Administrateur.php";
-require_once "Etudiant.php";
-require_once "Professeur.php";
-require_once "Secretariat.php";
+        }
 
-$type = $_POST["type"];
-$nom = $_POST["nom"];
-$prenom = $_POST["prenom"];
-$errorMessage = "";
-
-if($type == "secretariat"){
-    $pers = new Secretariat();
-    $pers->setRole('secretariat');
-}elseif ($type == "etudiant"){
-    $pers = new Etudiant();
-    $pers->setRole('etudiant');
-}elseif ($type == "prof"){
-    $pers = new Professeur();
-    $pers->setRole('professeur');
-}elseif ($type == "admin"){
-    $pers = new Administrateur();
-    $pers->setRole('administrateur');
-}
-
-$pwd = $_POST["pwd"];
-if($pers->verifMdp($pwd)){
-    $pers->setMdp($pwd);
-    $pers->setNom($nom);
-    $pers->setPrenom($prenom);
-    $id = $pers->genererIdentifiant();
-    echo "L'identifiant généré : ".$id."<br>";
-    $pers->insererDonnees();
-    echo "Mot de passe défini avec succès.";
-} else {
-    echo "Le mot de passe ne respecte pas les critères.";
-    echo '<a href="../../View/HTML/creationCompte.html">Réessayer</a>';
-}
-if ($errorMessage !== "" || $_SERVER["REQUEST_METHOD"] !== "POST") {
-
-    if ($errorMessage !== "") {
-        echo "<div style='color: red;'>$errorMessage</div>";
     }
-}?>
+}
+?>
