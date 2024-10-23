3b53bbe86e2ab4ce46e21004bf98e7cbb1d75066
Bastien2004 <bastienhecquet2004@gmail.com>
2024-10-23 12:16:29 +0200

    Debut Import

--- Model/Classe/creationCompte.php
+++ Model/Classe/creationCompte.php
@@ -1,11 +1,26 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $fichier = $_FILES["fichier"]["name"];
-    if (pathinfo($_FILES['fichier']['type'], PATHINFO_EXTENSION) === 'csv') {
-        if ((fopen($_FILES['fichier']['type'], "r")) !== FALSE){//Ouverture du fichier
+if (isset($_FILES['fichier']) && $_FILES['fichier']['error'] === UPLOAD_ERR_OK) {// Vérifier si le fichier a été téléchargé
+    $nomFichier = $_FILES['fichier']['name'];
 
+    // Vérifier si c'est un fichier CSV
+    if ($_FILES['fichier']['type'] === 'text/csv' || pathinfo($nomFichier, PATHINFO_EXTENSION) === 'csv') {
+        // Lire le fichier CSV
+        $lecture = fopen($_FILES['fichier']['tmp_name'], "r");
+        if ($lecture !== FALSE) {
+            $liste = [];
+            $res = fgetcsv($lecture, 1000, ",");
+            while ($res !== FALSE) {
+                $liste[] = $res; // Ajouter chaque ligne du CSV dans le tableau
+                echo htmlspecialchars($res[0]) . "<br>"; // Affiche la première colonne de chaque ligne pour vérifier
+            }
+            fclose($lecture);
+        } else {
+            echo "Impossible de lire le fichier.";
         }
-
+    } else {
+        echo "Le fichier n'est pas au format CSV.";
     }
+} else {
+    echo "Erreur lors du téléchargement du fichier.";
 }
 ?>
