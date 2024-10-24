8a73d97b1a38285660f3c9bce495e7ba7d131ada
Bastien2004 <bastienhecquet2004@gmail.com>
2024-10-24 10:27:56 +0200

    Debut Import

--- Model/Classe/creationCompte.php
+++ Model/Classe/creationCompte.php
@@ -1,10 +1,12 @@
 <?php
+include "../../Controller/ConnectionBDD.php";
+
 if (isset($_FILES['fichier']) && $_FILES['fichier']['error'] === UPLOAD_ERR_OK) {// Vérifier si le fichier a été téléchargé
     $nomFichier = $_FILES['fichier']['name'];
 
     // Vérifier si c'est un fichier CSV
     if ($_FILES['fichier']['type'] === 'text/csv' || pathinfo($nomFichier, PATHINFO_EXTENSION) === 'csv') {
-        echo 'ficier est un csv.<br>';
+        echo "ficier est un csv.<br>";
         // Lire le fichier CSV
         $lecture = fopen($_FILES['fichier']['tmp_name'], "r");
         if ($lecture !== FALSE) {
@@ -18,16 +20,15 @@ if (isset($_FILES['fichier']) && $_FILES['fichier']['error'] === UPLOAD_ERR_OK) 
                 if($testEmpty->fetchColumn()==0){ //Si la table est vide
                     echo 'table vide.<br>';
                     $liste = [];
-                    $res = fgetcsv($lecture, 1000, ",");
-                    while ($res !== FALSE) {
+                    while (($res = fgetcsv($lecture, 1000, ",")) !== FALSE) {
                         $liste[] = $res; // Ajouter chaque ligne du CSV dans le tableau
                         echo htmlspecialchars($res[0]) . "<br>"; // Affiche la première colonne de chaque ligne pour vérifier
                     }
                 } else{
                     echo 'table pas vide.<br>';
-                    while (($data = fgetcsv($lecture, 1000, ',')) !== FALSE) {
-                        $nom = $data[2];
-                        $prenom = $data[3];
+                    while (($res = fgetcsv($lecture, 1000, ",")) !== FALSE) {
+                        $nom = $res[2];
+                        $prenom = $res[3];
 
                         // Vérification ligne existant dans BDD
                         $sql1 = $conn->prepare("SELECT COUNT(*) FROM etudiants WHERE nom = :nom AND prenom = :prenom");
@@ -39,7 +40,7 @@ if (isset($_FILES['fichier']) && $_FILES['fichier']['error'] === UPLOAD_ERR_OK) 
                         if ($sql1->fetchColumn() == 0) { // Pas de doublon
                             echo 'insert des valeur.<br>';
                             $insertStmt = $conn->prepare("INSERT INTO etudiants (id, civilite, nom, prenom, semestre, nom_ressource, email) VALUES (:id, , :civilite, :nom, :prenom, :semestre, :email)");
-                            $insertStmt->execute(['id'=>($id=$data[0]),'civilite'=>($civilite=$data[1]), 'nom' => $nom, 'prenom' => $prenom, 'semestre'=>($semestre=$data[2]), 'nom_ressource'=> ($nom_ressource=$data[3]), 'email'=>($email= $data[4])]);
+                            $insertStmt->execute(['id'=>($id=$res[0]),'civilite'=>($civilite=$res[1]), 'nom' => $nom, 'prenom' => $prenom, 'semestre'=>($semestre=$res[2]), 'nom_ressource'=> ($nom_ressource=$res[3]), 'email'=>($email= $res[4])]);
                         }
                     }
                 }
