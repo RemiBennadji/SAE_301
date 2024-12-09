375360f115d0bd5db09608f02111fae278a1c0c1
Noah <syllektis59@gmail.com>
2024-12-09 11:47:02 +0100

    commencement html php de mdpOublie

--- Controller/mdpOublie.php
+++ Controller/mdpOublie.php
@@ -1,22 +1,15 @@
 <?php
 
-$id = $_POST["identifiant"];
-$pwd1 = $_POST['mdp'];
-$pwd2 = $_POST['mdp2'];
+$mail = $_POST["email"];
 
-$sql1="select identifiant, motdepasse from infoutilisateur where identifiant=:identifiant";
+
+$sql1="select identifiant, motdepasse from mailidentifiant JOIN infoutilisateur USING(identifiant) where mail=:mail";
 
 try {
     $conn = getConnectionBDDEDTIdentification();
     $res1 = $conn->prepare($sql1);
-    $res1->bindParam(":identifiant", $id);
+    $res1->bindParam(":mail", $mail);
     $res1->execute();
-    if ($pwd1 == $pwd2) {
-        if ($pwd1 != $res1[0]['motdepasse']) {
-
-        }
-
-    }
 
 } catch (PDOException $e) {
     $erreur = "Erreur SQL : " . $e->getMessage();
