21758a27ac89e3605d42bd0ac38a62dcca2c8af7
Bastien2004 <bastienhecquet2004@gmail.com>
2024-12-13 15:16:17 +0100

    fix

--- Controller/mdpOublie.php
+++ Controller/mdpOublie.php
@@ -5,6 +5,7 @@ include_once "../Model/Classe/Mail.php";
 
 function sendCode($email, $code, $conn){
 
+//    $conn = getConnectionBDDEDTIdentification();
     $time = strtotime("now");
     $sql1 = 'insert into codeverif values(email=:email, codeV=:code, date=:time)';
     try {
@@ -34,34 +35,63 @@ function sendCode($email, $code, $conn){
 //    $time = strtotime("now");
 //    $sql1 = 'insert into codeverif values(email::email, codeV::code, date::time)';
 //    try {
-//        $mail->isSMTP();
-//        $mail->Host = 'smtp.gmail.com';
-//        $mail->SMTPAuth = true;
-//        $mail->Username = 'saeedt301@gmail.com';
-//        $mail->Password = 'wxwxfhvqmswxufni';
-//        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-//        $mail->Port = 465;
-//        $mail->setFrom('saeedt301@gmail.com', 'Sae-EDT');
-//        $mail->addAddress($email);
-//
-//        $conn = getConnectionBDDEDTIdentification();
 //        $result = $conn->prepare($sql1);
 //        $result->bindParam(':email', $email);
 //        $result->bindParam(':code', $code);
 //        $result->bindParam(':time', $time);
 //        $result->execute();
 //
-//        $mail->isHTML(true);
-//        $mail->Subject = 'Code de Vérification';
-//        $mail->Body = 'Voici votre code de vérification : '.$code;
-//
-//        if($mail->send()){
-//            header('location: changeMDP.html');
+//        $mail = new Mail();
+//        $mail->setMdp('wxwxfhvqmswxufni');
+//        $mail->setDestinataire($email);
+//        $mail->setExpediteur('saeedt301@gmail.com');
+//        $mail->setObjet('Code de Vérification');
+//        $message = 'Voici votre code de vérification : '.$code;
+//        $mail->setMessage($message);
+//        $mail->setParam();
+//        $mail->creerMail();
+//        header('location: changeMDP.html');
 //            exit();
 //        }
 //    } catch (PDOException $e) {
 //        $erreur = "Erreur lors de l'envoi de l'email : " . $mail->ErrorInfo;
 //    }
+
+
+    $mail = new PHPMailer(true);
+    $time = strtotime("now");
+    $sql1 = 'insert into codeverif values(email::email, codeV::code, date::time)';
+    try {
+        $mail->SMTPDebug = 2;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'saeedts301@gmail.com';
+        $mail->Password = 'wxwxfhvqmswxufni';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
+        $mail->setFrom('saeedts301@gmail.com', 'Sae-EDT');
+        $mail->addAddress($email);
+
+//        $conn = getConnectionBDDEDTIdentification();
+//        $result = $conn->prepare($sql1);
+//        $result->bindParam(':email', $email);
+//        $result->bindParam(':code', $code);
+//        $result->bindParam(':time', $time);
+//        $result->execute();
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Code de Vérification';
+        $mail->Body = 'Voici votre code de vérification : '.$code;
+
+        if($mail->send()){
+            header('location: changeMDP.html');
+            exit();
+        }
+    } catch (PDOException $e) {
+        $erreur = "Erreur lors de l'envoi de l'email : " . $mail->ErrorInfo;
+    } catch (Exception $e) {
+    }
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
