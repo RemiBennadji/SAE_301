ee38c39b665a449f19f6b4950a92f80fa8796d0c
Noah <syllektis59@gmail.com>
2024-12-16 16:25:30 +0100

    maj js pour menu burger

--- Model/JavaScript/MenuPrincipal.js
+++ Model/JavaScript/MenuPrincipal.js
@@ -1,3 +1,31 @@
+function admin() {
+    document.getElementById("creationCompte").style.display = "block";
+}
+
+function etudiant() {
+}
+
+function professeur() {
+}
+
+function secretariat() {
+    document.getElementById("afficheSalles").style.display = "block";
+}
+$listeFinal = array();
+
+function afficherElement(role) {//Fonction qui verifie le rôle de l'utilisateur
+    if (role === "administrateur") {
+        admin();
+    } else if (role === "secretariat") {
+        secretariat();
+    } else if (role === "etudiant") {
+        etudiant();
+    } else if (role === "professeur") {
+        professeur();
+    }
+    console.log(role);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const edtAdmin = document.getElementById('edtAdmin');
     if (edtAdmin) {
