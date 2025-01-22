6794e58480818b33d85f8fd5ddbb490f293deb55
AnsseauMattheo <ansseaumattheo1@gmail.com>
2025-01-22 11:06:02 +0000

    Test changement connexion vers BDD serveur

--- Controller/ConnectionBDD.php
+++ Controller/ConnectionBDD.php
@@ -3,14 +3,15 @@
 function getConnectionBDD(){
     {
         try {
-            $dbname = "edt";
-            $user = "iutinfo301";
-            $password = "YAH+rfI3";
-            $dsn = "pgsql:host=iutinfo-sgbd.uphf.fr;dbname=$dbname";
+//            $dbname = "edt";
+//            $user = "iutinfo301";
+//            $password = "YAH+rfI3";
+//            $dsn = "pgsql:host=iutinfo-sgbd.uphf.fr;dbname=$dbname";
 
-//            $dsn = "pgsql:host=2a02:842a:81db:d601:88a7:f394:4625:e9ff;dbname=sae";
-//            $user = "lecteur";
-//            $password = "root";
+            $dsn = "jdbc:postgresql://192.168.38.45:5432/sae301";
+	    $dbname="sae301";
+            $user = "postgres";
+            $password = "root";
 
             $pdo = new PDO($dsn, $user, $password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
