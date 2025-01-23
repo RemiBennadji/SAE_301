2ced623c64db7e935aeb336ffd324e1461ed610c
Mattheo <ansseaumattheo1@gmail.com>
2025-01-23 18:29:08 +0100

    fix

--- Controller/TableauAbsence.php
+++ Controller/TableauAbsence.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $dateActuelle = new DateTime(); // Récupérer la date actuelle sous forme d'objet DateTime
 $dateActuelle->modify('monday this week'); // Définir la date sur le lundi de la semaine actuelle
 
@@ -35,23 +37,53 @@ if (isset($_POST['precedent'])) {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.22/jspdf.plugin.autotable.min.js"></script>
 <header>
+    <!-- Menu de navigation principal -->
     <nav>
+        <!-- Menu burger (pour affichage mobile) -->
         <div class="burger">
             <span></span>
             <span></span>
             <span></span>
         </div>
         <ul class="menu">
-            <li><a id="edtCours" class="underline-animation" href="../Controller/EDTmatiereSelection.php" style="display: none">EDT Ressource</a></li>
+            <!-- Lien vers différentes sections du site, avec affichage conditionnel -->
             <li><a id="edtProf" class="underline-animation" href="../Controller/EDTprof.php" style="display: none">EDT Professeur</a></li>
-            <li><a id="edt" class="underline-animation" href="../Controller/EDT.php">Emploi du temps</a></li>
-            <li><a class="underline-animation" href="#">Messagerie</a></li>
-            <li><a class="underline-animation" href="../View/HTML/creationCompte.html" id="creationCompte" style="display: none">Créer un compte</a></li>
+            <li><a id="edtCours" class="underline-animation" href="../Controller/EDTmatiereSelection.php" style="display: none">EDT Ressource</a></li>
             <li><a class="underline-animation" href="../Controller/EDTsalleLibres.php" id="afficheSalles">Salles disponibles</a></li>
+            <li><a id="tableauEtudiant" class="underline-animation" href="../Controller/VoireEtudiant.php" style="display: none">Listes Étudiants</a></li>
+            <li><a id="tableauAbsence" class="underline-animation" href="../Controller/TableauAbsence.php" style="display: none">Tableau Absence</a></li>
+            <li><a class="underline-animation" href="../View/HTML/demandePage.php" id="demande" style="display: none">Faire une demande</a></li>
+            <li><a class="underline-animation" href="../View/HTML/creationCompte.html" id="creationCompte" style="display: none">Créer un compte</a></li>
+            <li><a id ="valideEDT" class="underline-animation" href="../Controller/ValideEdt.php" style="display: none">ValideEDT</a></li>
+            <!-- Sélecteur d'année scolaire, affiché conditionnellement -->
+            <label class="choixClasse" id="choixClasse" style="display: none">
+                <select id="edtAdmin" class="edtAdmin">
+                    <option selected disabled>Choisir Année</option>
+                    <!-- Options pour l'année scolaire -->
+                    <option class="label" disabled>Année 1</option>
+                    <option value="A1">A1</option>
+                    <option value="A2">A2</option>
+                    <option value="B1">B1</option>
+                    <option value="B2">B2</option>
+                    <option value="C1">C1</option>
+                    <option value="C2">C2</option>
+                    <option class="label" disabled>Année 2</option>
+                    <option value="FIA1">FIA1</option>
+                    <option value="FIA2">FIA2</option>
+                    <option value="2FIB">FIB</option>
+                    <option value="2FA">FA</option>
+                    <option class="label" disabled>Année 3</option>
+                    <option value="FIA">FIA</option>
+                    <option value="FIB">FIB</option>
+                    <option value="FA">FA</option>
+                </select>
+            </label>
             <li><a class="underline-animation" href="../Controller/Deconnexion.php">Déconnexion</a></li>
         </ul>
     </nav>
 </header>
+
+<!-- Script pour faire fonctionner le menu burger (affichage mobile) -->
 <script>
     const burger = document.querySelector('.burger');
     const menu = document.querySelector('.menu');
@@ -85,8 +117,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Vérifier si le cookie "groupe" existe
-session_start();
+
 
 if (!isset($_SESSION['role'])) {
     header("Location: ../View/HTML/Deconnexion.html"); // Redirection si pas de rôle
