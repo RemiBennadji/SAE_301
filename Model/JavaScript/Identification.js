cda29b7ffd36de3677d3ffe3477a27495cc96768
matthéo <ansseaumattheo1@gmail.com>
2025-03-05 12:00:40 +0100

    Message erreur lors de la connexion avec css/html/JS fait @mattheo

--- Model/JavaScript/Identification.js
+++ Model/JavaScript/Identification.js
@@ -5,6 +5,8 @@ const idpsw = document.getElementById('idpsw');
 const labelIdentifiant = document.getElementById('labelIdentifiant')
 const labelPsw = document.getElementById('labelPsw')
 const yeux = document.getElementById('passwordSymbole')
+const textId = document.getElementById('idFaux')
+const textMdp = document.getElementById('mdpFaux')
 
 document.getElementById('formID').addEventListener('submit', function (event) {
     console.log('Gestionnaire de soumission appelé');
@@ -25,16 +27,29 @@ document.getElementById('formID').addEventListener('submit', function (event) {
                 if (jsonData.error){
                     console.error(jsonData.error);
 
-                    if (jsonData.error === 'errorConnexion') {
+                    if (jsonData.error === 'error identifiant') {//erreur si identifiant incorrect
                         // Change le style en cas d'échec
                         idcompte.style.background = '#f2a19b';
                         idcompte.style.border = '2px solid red';
                         labelIdentifiant.style.color = 'red';
-
                         idpsw.style.background = '#f2a19b';
                         idpsw.style.border = '2px solid red';
                         labelPsw.style.color = 'red';
                         yeux.style.color = 'black';
+                        textMdp.style.display = 'none';
+                        textId.style.display = 'block';
+                    }
+                    if (jsonData.error === 'errorMDP' ) {//erreur si MDP incorrect
+                        // Change le style en cas d'échec
+                        idcompte.style.background = '#f2a19b';
+                        idcompte.style.border = '2px solid red';
+                        labelIdentifiant.style.color = 'red';
+                        idpsw.style.background = '#f2a19b';
+                        idpsw.style.border = '2px solid red';
+                        labelPsw.style.color = 'red';
+                        yeux.style.color = 'black';
+                        textId.style.display = 'none';
+                        textMdp.style.display = 'block';
                     }
                 }else if (jsonData.redirect) {
                     // Effectue la redirection vers l'URL fournie par le PHP
